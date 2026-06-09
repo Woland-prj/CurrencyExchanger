ca62107152ffb89f8f494adad29c2b0bf6cb8dbc
Woland-prj <nzimin93@gmail.com>
2026-06-09 17:20:53 +0300

    fix server error text to more informative

--- client/src/components/widgets/CurrencyConverter/CurrenciesError/CurrenciesError.tsx
+++ client/src/components/widgets/CurrencyConverter/CurrenciesError/CurrenciesError.tsx
@@ -9,7 +9,7 @@ export const CurrenciesError = ({ error }: CurrenciesErrorProps) => {
   return (
     <Card>
       <div className={styles.error}>
-        <p className={styles.message}>Server is unavailable. Please try again later.</p>
+        <p className={styles.message}>Could not get data from the server. Please try again later.</p>
         <p className={styles.detail}>{error}</p>
       </div>
     </Card>
